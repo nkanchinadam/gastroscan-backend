7f83efd97f02edb9630432d0abcd7ef0be0630a8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-03-29 08:25:19 -0400

    wrote code to deal receive image from frontend and run it through a model that is being hosted on Flask

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 const cors = require("cors");
+const fetch = require("node-fetch");
 app.use(cors());
 
 const bodyParser = require("body-parser");
@@ -11,25 +12,20 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({limit: '50mb'}));
 
-const tfjs = require("@tensorflow/tfjs");
-
-app.get('/test_endpoint', async (req, res) => {
-	const model = await tfjs.loadLayersModel('C:/Nitin/School/High School/TJHSST/Senior Year/Senior Research Lab/Website/gastroscan-backend/public/models/abnormality_model.json');
-	console.log(typeof model);
-	console.log('model', model);
-	const tensor = tfjs.tensor(Array(100).fill(new Array(100).fill(new Array(3).fill(0))));
-	console.log('tensor', tensor);
-	const prediction = await model.predict(tensor);
-	console.log(prediction);
-});
-
-app.post('/test_endpoint', (req, res) => {
-	console.log(req.body);
-});
-
-app.post('/handle_file_upload', (req, res) => {
-	console.log(req.body);
-	res.send('ok');
+app.post('/handle_file_upload', async (req, res) => {
+	const pixels = req.body.pixels
+	const result = await fetch('http://localhost:5000/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			tensor: [pixels],
+			modelType: req.body.modelType
+		})
+	});
+	const response = await result.json();
+	res.send(response);
 });
 
 try {
