cfb8409a2d63fd96d056a2d1627aeb87aacd959f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-05-20 13:15:29 -0400

    added a semicolon to the end of a line

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({limit: '50mb'}));
 
 app.post('/handle_file_upload', async (req, res) => {
-	const pixels = req.body.pixels
+	const pixels = req.body.pixels;
 	const result = await fetch('http://localhost:5000/', {
 		method: 'POST',
 		headers: {
